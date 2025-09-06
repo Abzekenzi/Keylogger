ab783a58343c22549d4a75741dfb5b82c6b7d451
your name <you@example.com>
2025-09-06 06:43:54 -0400

    Added file logging

--- listener.py
+++ listener.py
@@ -11,9 +11,14 @@ def log_bytearray_to_file(buffer: bytearray, filename: str):
     with open(filename, "ab") as f:  # append in binary mode
         f.write(buffer.replace(b"\n", b" ") + b"\n")
 
+def beautify_log(filename):
+    file = open(filename, 'r')
+
+
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    buffer = bytearray()
+    buffer = ""
+    output_file = open("log.txt", 'a', encoding="utf-8")
 
     host = '127.0.0.1'
     port = 4444
@@ -27,14 +32,16 @@ def main():
         if len(buffer) <= BUFFER_SIZE:
             data = conn.recv(1)
             if data:
-                buffer.extend(data)
+                buffer += data.decode("utf-8")
             else:
                 print("No data has been received")
+                beautify_log("log.txt")
                 break
         else:
-            print(buffer.decode().replace("''", ""))
-            buffer.clear()
-
+            print(buffer)
+            output_file.write(buffer.replace("'", ""))
+            buffer = ""
+    output_file.close()
 
 
 if __name__ == "__main__":
